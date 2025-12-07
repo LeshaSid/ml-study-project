5e8569d2a34deed88c8af288fe0b1f5328f577a7
LeshaSid <lesha.sidoruk@gmail.com>
2025-12-08 01:05:06 +0300

    menu and stock price upgrade

--- pages/stock_price_forecast.py
+++ pages/stock_price_forecast.py
@@ -1,6 +1,5 @@
 import streamlit as st 
 import pandas as pd 
-import numpy as np 
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
